2a0b21560b3d0e79d9af3feafaedffbc7fd09814
ZHANG YIMING <47180638+ZHymLumine@users.noreply.github.com>
2023-09-25 14:25:14 +0900

    add data

---
+++ utils.py
@@ -0,0 +1,25 @@
+import os
+import time
+import shutil
+
+
+class Timer():
+
+    def __init__(self):
+        self.v = time.time()
+
+    def s(self):
+        self.v = time.time()
+
+    def t(self):
+        return time.time() - self.v
+
+
+def time_text(t):
+    if t >= 3600:
+        return '{:.1f}h'.format(t / 3600)
+    elif t >= 60:
+        return '{:.1f}m'.format(t / 60)
+    else:
+        return '{:.1f}s'.format(t)
+
